cca62d6b326e73abc309d8b72e80eb8b6def4a64
Athipat Pingwong <athipat.pingwong@mail.kmutt.ac.th>
2021-02-03 11:11:09 +0700

    group work 2 update 1

---
+++ profile.js
@@ -0,0 +1,19 @@
+    const app = {
+        data() {
+            return {
+                name: 'Athipat Pingwong',
+                job: 'IT Student',
+                head1: 'Year',
+                head2: 'Credit',
+                head3: 'Grade',
+                data1: '2nd',
+                data2: '51',
+                data3: '2',
+                left: 'dislike',
+                right: 'like',
+                image: '/images/tempura.png'
+            }
+        }
+
+    }
+   const mountedApp = Vue.createApp(app).mount('#app')
